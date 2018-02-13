d6c854ba6e7d888a456f5251e0ad1e3b70c33b1f
Nikolay Fedyukin <nick_fed@mail.ru>
2018-02-13 21:10:03 +0300

    первый коммит

---
+++ task1.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Задание 1</title>
+</head>
+<body>
+<h1>Задание №1</h1>
+
+
+<br><br>
+<a href="/Homework3/index.html">Назад</a>
+</body>
+</html>
