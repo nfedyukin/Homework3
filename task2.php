ccf9d1c544273f130660346b7f2d20139c748373
Nikolay Fedyukin <nick_fed@mail.ru>
2018-02-18 22:00:43 +0300

    Задания 1,2,3

--- task2.php
+++ task2.php
@@ -4,7 +4,7 @@ $file_comments = __DIR__ . '\data\comments.txt';
 $comments = file($file_comments);
 
 if($_POST['comment']){
-    array_push($comments, PHP_EOL . $_POST['comment']);
+    array_push($comments, PHP_EOL . str_replace(PHP_EOL, ' ',$_POST['comment']));
     file_put_contents($file_comments, $comments);
     $comments = file($file_comments);
 }
@@ -17,6 +17,7 @@ if($_POST['comment']){
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="style.css" rel="stylesheet">
     <title>Задание 2</title>
 </head>
 <body>
@@ -27,7 +28,7 @@ if($_POST['comment']){
 if (!empty($comments)){
     foreach ($comments as $comment){
         ?>
-        <blockquote><article style="border: 1px solid gray">
+        <blockquote><article style="border: solid 2px #DBCECE" ">
                 <?php echo $comment; ?>
             </article></blockquote>
         <?php
@@ -35,10 +36,14 @@ if (!empty($comments)){
 }
 ?>
 
-<form style="border: 1px solid black" method="post">
-    <textarea style="width: 80%; margin: 15px 15px 15px 15px" name="comment"  rows="5"></textarea><br>
-    <button type="submit" style="margin: 0px 15px 15px 15px">Добавить коментарий</button><br>
-</form>
+<div style="width: 500px">
+    <form style="border: 1px solid black" method="post" >
+        <textarea style="width: 80%; margin: 15px 15px 15px 15px" name="comment"  rows="5"></textarea><br>
+        <button type="submit" style="margin: 0px 15px 15px 15px">Добавить коментарий</button><br>
+    </form>
+</div>
+<br><br>
+<a href="/Homework3/index.html">Назад</a>
 
 
 </body>
--- task3.php
+++ task3.php
@@ -18,29 +18,67 @@ if (isset($_FILES['image'])){
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="style.css" rel="stylesheet">
     <title>Задание 3</title>
+    <style>
+        .button {
+            background-color: #4CAF50; /* Green */
+            border: none;
+            color: white;
+            padding: 16px 32px;
+            text-align: center;
+            text-decoration: none;
+            display: inline-block;
+            font-size: 12px;
+            margin: 4px 2px;
+            -webkit-transition-duration: 0.4s; /* Safari */
+            transition-duration: 0.4s;
+            cursor: pointer;
+        }
+
+        .button1 {
+            background-color: white;
+            color: black;
+            border: 2px solid #4CAF50;
+        }
+
+        .button1:hover {
+            background-color: #4CAF50;
+            color: white;
+        }
+
+    </style>
 </head>
 <body>
+
 <h1>Задание №3</h1>
 <h2>Фотогалерея</h2>
+<div style="width: 300px">
+    <form style="border: 2px solid #4CAF50" method="post" enctype="multipart/form-data" >
+        <!--<input type="hidden" name="MAX_FILE_SIZE" value="1000">-->
+        <h3 style="margin-left: 10px">Добавить рисунок: </h3>
+        <input type="button" style="margin-left: 10px" class="button button1" id="loadFile" value="1. Выбрать рисунок" onclick="document.getElementById('image').click();" />
+        <input type="file" style="display:none;" id="image" name="image" accept="image/jpeg, image/png"><br>
+        <button type="submit" style="margin-left: 10px" class="button button1">2. Добавить в библиотеку</button>
+    </form>
+</div>
 
-
-
-<form style="border: 1px solid black" method="post" enctype="multipart/form-data" >
-    Выберите картинку для загрузки: <input type="file" name="image"><br>
-    <button type="submit" style="margin: 0px 15px 15px 15px">Добавить</button>
-</form>
-
+<br>
+<hr style="border: 1px solid #4CAF50">
 <?php
-$img_list = array_slice(scandir($img_folder), 2);
+$img_list = preg_grep('~\.(jpeg|jpg|png)$~',array_slice(scandir($img_folder), 2));
 //var_dump($img_list);
 
 foreach ($img_list as $image){
     ?>
-    <img src="<?php echo '/Homework3/img/' . $image ?>"><br>
+    <img id="shadowbox" style="width: auto" src="<?php echo '/Homework3/img/' . $image ?>"><br><br>
+
     <?php
 }
 ?>
+<br><br>
+<a href="/Homework3/index.html">Назад</a>
+
 
 </body>
 </html>
